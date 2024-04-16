7776ab94dfbee708ab6d882731d9b23b67a20043
Stephen Cooper <scooper@indeed.com>
2024-04-16 15:13:28 -0500

    First commit

---
+++ Day_2/Homework/LeagueManager/src/main/java/org/example/leaguemanager/LeagueManagerApplication.java
@@ -0,0 +1,13 @@
+package org.example.leaguemanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeagueManagerApplication.class, args);
+    }
+
+}
---
+++ Day_2/Homework/LeagueManager/src/test/java/org/example/leaguemanager/LeagueManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.leaguemanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
